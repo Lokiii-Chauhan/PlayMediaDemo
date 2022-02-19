1f403e9313c17284a7845c05a8260e7d5fd50306
lokii <lokiiichauhan@gmail.com>
2022-02-19 13:23:10 +0530

    added seekbar

--- app/src/main/java/com/lokiiichauhan/playmediademo/MainActivity.java
+++ app/src/main/java/com/lokiiichauhan/playmediademo/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private String url = "https://pagalsong.in/uploads/systemuploads/mp3/Ae%20Dil%20Hai%20Mushkil/Ae_Dil_Hai_Mushkil_Title_Song_320_Kbps.mp3";
     private MediaPlayer mediaPlayer;
     private Button playButton;
+    private SeekBar seekBar;
 
 
     @Override
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         playButton = findViewById(R.id.play_button);
+        seekBar = findViewById(R.id.seekBarId);
 
         mediaPlayer = new MediaPlayer();
         try {
@@ -29,10 +33,18 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                int duration = mp.getDuration();
+                Toast.makeText(MainActivity.this, String.valueOf((duration / 1000) / 60),Toast.LENGTH_LONG).show();
+            }
+        });
         MediaPlayer.OnPreparedListener preparedListener = new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
+                seekBar.setMax(mp.getDuration());
+
                 playButton.setOnClickListener(view ->{
                     if (mp.isPlaying()){
                         mp.pause();
@@ -46,8 +58,27 @@ public class MainActivity extends AppCompatActivity {
         };
 
         mediaPlayer.setOnPreparedListener(preparedListener);
-
         mediaPlayer.prepareAsync();
+
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser){
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
 
     }
